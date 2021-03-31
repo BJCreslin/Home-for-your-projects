2cdffcf423b3dd2c55b0bdf93d8cafd069b35abe
bjcreslin <bjcreslin@gmail.com>
2021-03-31 14:17:30 +0700

    rename header

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -15,7 +15,7 @@ export const BrandIcon = props => (
 export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <BrandIcon />
-    <span className="brand-title">HomeForYourProjects</span>
+    <span className="brand-title">Home For Your Projects</span>
     <span className="navbar-version">{appConfig.VERSION}</span>
   </NavbarBrand>
 );
