4d1b2de79fe80d65e4550b3f2f6a809fc7d1969a
BJCreslin <bjcreslin@gmail.com>
2021-04-04 19:45:58 +0700

    added license

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -8,7 +8,9 @@ const Footer = props => (
   <div className="footer page-content">
     <Row>
       <Col md="12">
-        <p>2021 bjcreslin@gmail.com</p>
+        <p>
+          2021. e-mail:bjcreslin@gmail.com. <a href="http://www.apache.org/licenses/LICENSE-2.0">APACHE LICENSE, VERSION 2.0</a>
+        </p>
       </Col>
     </Row>
   </div>
