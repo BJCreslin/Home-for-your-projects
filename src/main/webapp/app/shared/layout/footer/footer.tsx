babfdccf5d9a6b27bcb2af538820007e3b946dbb
BJCreslin <bjcreslin@gmail.com>
2021-04-04 21:11:16 +0700

    added mailto

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -9,7 +9,8 @@ const Footer = props => (
     <Row>
       <Col md="12">
         <p>
-          2021. e-mail:bjcreslin@gmail.com. <a href="http://www.apache.org/licenses/LICENSE-2.0">APACHE LICENSE, VERSION 2.0</a>
+          2021. <a href="mailto:bjcreslin@gmail.com">E-mail: bjcreslin@gmail.com</a>{' '}
+          <a href="http://www.apache.org/licenses/LICENSE-2.0">APACHE LICENSE, VERSION 2.0</a>
         </p>
       </Col>
     </Row>
